847998d6b5a3296e89059613fcbaaff42db852e1
Ayush Pandey <64825858+iayushpandey@users.noreply.github.com>
2022-07-20 13:02:42 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -38,8 +38,7 @@ export default function TextForm(props) {
     }
 
     const [text, setText] = useState(''); 
-    // text = "new text"; // Wrong way to change the state
-    // setText("new text"); // Correct way to change the state
+   
     return (
         <>
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
